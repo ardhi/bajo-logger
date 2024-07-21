824e57474ea8f79d2e79e851118d778791f43d30
Ardhi Lukianto <ardhi@lukianto.com>
2024-07-21 07:28:13 +0700

    Updates

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # bajo-logger
+
+Plugin name: **bajoLogger**, alias: **logger**
+
+![GitHub package.json version](https://img.shields.io/github/package-json/v/ardhi/bajo-logger) ![NPM Version](https://img.shields.io/npm/v/bajo-logger)
+
+> <br />**Attention**: I do NOT accept any pull request at the moment, thanks!<br /><br />
+
+Pino logger for [Bajo](https://github.com/ardhi/bajo)
+
+## Installation
+
+Goto your ```<bajo-base-dir>``` and type:
+
+```bash
+$ npm install bajo-logger
+```
+
+Now open your ```<bajo-data-dir>/cron/.plugins``` and put ```bajo-logger``` in it
+
+## License
+
+[MIT](LICENSE)
--- bajo/init.js
+++ bajo/init.js
@@ -1,40 +1,37 @@
 import pino from 'pino'
 
 async function init () {
-  const { getConfig, logLevels, log } = this.bajo.helper
-  const { get, set, forOwn, isEmpty } = this.bajo.helper._
-  const self = this
-  const config = getConfig()
-  const opts = getConfig('bajoLogger').log ?? {}
-  opts.level = config.log.level
+  const { logLevels } = this.app.bajo
+  const { sprintf } = this.app.bajo.lib
+  const { get, set, forOwn, isEmpty, isPlainObject } = this.app.bajo.lib._
+  const i18n = this.app.bajoI18N
+  const me = this
+  const opts = this.getConfig().log ?? {}
+  opts.level = this.app.bajo.config.log.level
   if (get(opts, 'transport.target') === 'pino-pretty') {
     set(opts, 'transport.options.translateTime', 'UTC:yyyy-mm-dd\'T\'HH:MM:ss.l\'Z\'')
   }
-  this.bajoLogger.instance = pino(opts)
+  this.instance = pino(opts)
   const logger = {
     child: function () {
       const child = Object.create(this)
-      child.pino = self.bajoLogger.instance.child({})
+      child.pino = me.instance.child({})
       return child
     }
   }
   forOwn(logLevels, (v, k) => {
     logger[k] = (...args) => {
-      const [data, msg, ...rest] = args
-      /*
+      let [data, msg, ...rest] = args
       if (i18n) {
-        let ns = rest[0]
-        if (ns === 'bajo') ns = 'bajoI18N'
-        if (isPlainObject(rest[1])) msg = i18n.t(msg, merge({ ns }, rest[1]))
-        else msg = i18n.t(msg, { ns, postProcess: 'sprintf', sprintf: null })
-      }
-      */
+        if (isPlainObject(args[0])) msg = i18n.t(msg, rest[0])
+        else msg = i18n.t(msg, { ns: me.name, postProcess: 'sprintf', sprintf: rest })
+      } else msg = sprintf(msg, ...rest)
       const params = isEmpty(data) ? [msg, ...rest] : [data, msg, ...rest]
-      this.bajoLogger.instance[k](...params)
+      this.instance[k](...params)
     }
   })
-  this.bajoLogger.logger = logger
-  log.debug('Switched to \'Pino\' logger')
+  this.logger = logger
+  this.log.debug('Switched to \'%s\' logger', 'Pino')
 }
 
 export default init
